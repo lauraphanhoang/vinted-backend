37a9b80d139c293ee63aa1dc0d3de31a9fb14837
Laura Phan Hoang <lauraphanhoang@BN-MacBook.local>
2024-11-01 16:00:07 +0100

    add cors

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+const cors = require("cors");
+
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 app.use(express.json());
+app.use(cors());
 require("dotenv").config();
 
 mongoose.connect(process.env.MONGODB_URI);
