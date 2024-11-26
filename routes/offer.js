fca21f739b0a05abbef938856bc9c3bde2ab49ef
Laura Phan Hoang <lauraphanhoang@BN-MacBook.local>
2024-11-26 22:16:38 +0100

    vinted backend cleaned

--- routes/offer.js
+++ routes/offer.js
@@ -18,21 +18,12 @@ router.post(
   async (req, res) => {
     try {
       const convertedPicture = convertToBase64(req.files.picture);
-      // conversion de l'image grâce à la fonction 'convertToBase64(file)'
-      // console.log(req.files.picture); pour visualiser les files
-      // console.log(convertedPicture); pour visualiser les files converties
 
-      // envoi de l'image dans 'cloudinary', dans un dossiers 'offers', lui même placé dans un dossier 'vinted'
       const result = await cloudinary.uploader.upload(convertedPicture, {
         folder: "vinted/offers",
       });
       // console.log(result);
 
-      // LE DESTRUCTURING : je crée des variables et je stocke pour un gain de temps
-      // const { title, description, price, brand, city, size, condition, color } = req.body;
-      //console.log(title, description, price, brand, city, size, condition, color);
-
-      // création de la nouvelle offre à enregistrer en base de données (MongoDB)
       const newOffer = new Offer({
         product_name: req.body.title,
         product_description: req.body.description,
@@ -45,11 +36,10 @@ router.post(
           { EMPLACEMENT: req.body.city },
         ],
         owner: req.user,
-        product_image: result, // envoi de l'objet de l'image, venant de 'cloudinary'
+        product_image: result,
       });
-      await newOffer.save(); // envoi de la nouvelle offre complète en base de données (MongoDB)
+      await newOffer.save();
 
-      // création de la variable permettant d'afficher les informations souhaitées dans la réponse
       const dataToDisplay = await Offer.find().populate(
         "owner",
         "account username, _id"
@@ -62,68 +52,54 @@ router.post(
   }
 );
 
-// router.get("/offers", async (req, res) => {
-//   try {
-//     //destructuring :
-//     const { title, priceMin, priceMax, sort, page } = req.query;
+router.get("/offers", async (req, res) => {
+  try {
+    const { title, priceMin, priceMax, sort, page } = req.query;
+    const limit = 5;
+    const filters = {};
+    const regExp = new RegExp(title, "i");
 
-//     // console.log(title, priceMin, priceMax, sort, page);
-//     const limit = 5;
-//     const filters = {}; // si j'ai un title => {product_name : new RegEx(title,"i")}
-//     const regExp = new RegExp(title, "i");
-//     // si on m'a envoyé un titre alors j'ajouter à filters une clé product_name qui contient ma regexp
-//     if (title) {
-//       filters.product_name = new RegEx(title, "i");
-//     }
+    if (title) {
+      filters.product_name = new RegEx(title, "i");
+    }
 
-//     // si j'ai priceMin => {product_price : {$gte: Number(priceMin)}
-//     if (priceMin) {
-//       filters.product_price = { $gte: Number(priceMin) };
-//     }
+    if (priceMin) {
+      filters.product_price = { $gte: Number(priceMin) };
+    }
 
-//     // si j'ai priceMax => {product_price : {$lte: Number(priceMax)}
-//     if (priceMax) {
-//       //attention : si priceMin existe ! L'objet product_price existe déjà !
-//       // il ne faut donc pas l'écraser !
-//       if (priceMin) {
-//         filters.product_price.$lte = Number(priceMax);
-//       } else {
-//         filters.product_price = { $lte: Number(priceMax) };
-//       }
-//     }
+    if (priceMax) {
+      if (priceMin) {
+        filters.product_price.$lte = Number(priceMax);
+      } else {
+        filters.product_price = { $lte: Number(priceMax) };
+      }
+    }
 
-//     //SORTS
-//     const sortObj = {};
-//     if (sort === "price-asc") {
-//       sortObj.product_price = "asc";
-//     } else if (sort === "price-desc") {
-//       sortObj.product_price = "desc";
-//     }
+    //SORTS
+    const sortObj = {};
+    if (sort === "price-asc") {
+      sortObj.product_price = "asc";
+    } else if (sort === "price-desc") {
+      sortObj.product_price = "desc";
+    }
 
-//     // par défaut je suis à la page 1
-//     let skip = 0;
-//     // page 1 => 0*limit / (page-1*limit) => skip 0
-//     // page 2 => 1*limit / (page-1*limit) => skip 5
-//     // page 3 => 2*limit / (page-1*limit) => skip 10
-//     // SI J'AI UNE PAGE INDIQUÉE => alors je change skip pour => (page - 1) * limit
+    let skip = 0;
 
-//     if (page) {
-//       skip = (page - 1) * limit;
-//     }
-//     // console.log(filters);
+    if (page) {
+      skip = (page - 1) * limit;
+    }
 
-//     const offers = await Offer.find({ filters })
-//       .sort(sortObj)
-//       .skip(skip)
-//       .limit(limit)
-//       .populate("owner", "account");
-//     // .select("product_name product_price");
+    const offers = await Offer.find({ filters })
+      .sort(sortObj)
+      .skip(skip)
+      .limit(limit)
+      .populate("owner", "account");
 
-//     return res.json(offers);
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({ message: error.message });
-//   }
-// });
+    return res.json(offers);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
--- routes/user.js
+++ routes/user.js
@@ -13,7 +13,6 @@ router.post("/user/signup", async (req, res) => {
   try {
     // console.log(req.body)
 
-    //ai-je tout ? s'il manque username, email ou password
     if (!req.body.username || !req.body.email || !req.body.password) {
       res.status(400).json({ message: "Missing parameters" });
     }
@@ -24,14 +23,11 @@ router.post("/user/signup", async (req, res) => {
       res.status(400).json({ message: "Your account alrealy exist" });
     }
 
-    // 1) const salt = uid2(16);
     const salt = uid2(16);
     const token = uid2(64);
 
-    // 2) hashing
     const hash = SHA256(req.body.password + salt).toString(encBase64);
 
-    //3) on enregistre le user
     const newUser = new User({
       email: req.body.email,
       account: {
@@ -42,7 +38,7 @@ router.post("/user/signup", async (req, res) => {
       hash: hash,
       salt: salt,
     });
-    // console.log(newUser);
+
     await newUser.save();
 
     res.status(201).json({
@@ -50,11 +46,6 @@ router.post("/user/signup", async (req, res) => {
       token: newUser.token,
       account: newUser.account,
     });
-
-    /* ou autre méthode pour n'afficher que les éléments voulus :
-  const userDisplay = await User.findOne(newUser).select("_id token account username");
-    res.status(201).json(userDisplay);
- */
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
@@ -63,23 +54,13 @@ router.post("/user/signup", async (req, res) => {
 
 router.post("/user/login", async (req, res) => {
   try {
-    //console.log(req.body); // {email : `johndoe@lereacteur.io`, password : azerty`}
-    const userFound = await User.findOne({ email: req.body.email });
-    // console.log(userFound)
-
     if (!userFound) {
-      // si l'email n'existe pas
       res.status(400).json({ message: "email incorrect" });
     }
 
     const newHash = SHA256(req.body.password + userFound.salt).toString(
       encBase64
     );
-
-    //   console.log(userFound.hash)
-    //   console.log(newHash)
-
-    // si le hash est !== au nouveau hash c'est le mauvais mdp
     if (userFound.hash !== newHash) {
       res.status(400).json({ message: "wrong password" });
     }
@@ -89,10 +70,6 @@ router.post("/user/login", async (req, res) => {
       token: userFounf.token,
       account: userFound.account,
     });
-    // const userDisplay = await User.findOne({
-    //   email: req.body.email,
-    // }).select("_id token account");
-    // return res.status(201).json(userDisplay);
   } catch (error) {
     console.log(error);
     res.status(500).json({ message: error.message });
